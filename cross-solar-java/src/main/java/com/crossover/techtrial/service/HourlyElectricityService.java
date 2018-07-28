3e79e5da34fd56d9ccbb4cb8d4d9499fa20e8fb6
Hiren Patel <hiren.patel@ihg.com>
2018-07-29 03:02:59 +0530

    My submission

---
+++ cross-solar-java/src/main/java/com/crossover/techtrial/service/HourlyElectricityService.java
@@ -0,0 +1,22 @@
+package com.crossover.techtrial.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.crossover.techtrial.model.HourlyElectricity;
+
+/**
+ * HourlyElectricityService interface for all services realted to HourlyElectricity.
+ * @author Crossover
+ *
+ */
+public interface HourlyElectricityService {
+  HourlyElectricity save(HourlyElectricity hourlyElectricity);
+  
+  Page<HourlyElectricity> getAllHourlyElectricityByPanelId(Long panelId, Pageable pageable);
+  
+  List<HourlyElectricity> getAllHourlyElectricityByPanelIdAndDate(Long panelId, LocalDateTime date);
+}
